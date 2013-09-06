20c87d68b48b56f02f7633a0437795e43b428580
ssarahh <sitisarahsuryaningtyas@gmail.com>
2013-09-06 15:20:30 +0700

    sesi2

---
+++ Person.java
@@ -0,0 +1,14 @@
+// ini adalah nama class
+public class Person{
+	//ini adalah main class
+	public static void main(String[]  abc){
+		//ini adalah statement
+		System.out.println("Halo Dunia");
+ 	}
+/*
+*bla bla bla
+*bla bla bla
+*/
+}
+
+class Sarah
